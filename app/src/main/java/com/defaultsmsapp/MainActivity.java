5eb980ea008206c5688d232a51f2028995a8bfb8
Amira Hozaifa <you@example.com>
2017-08-01 13:54:06 +0200

    adding java

---
+++ app/src/main/java/com/defaultsmsapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.defaultsmsapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.defandsmsapp.R;
+
+/**
+ * Created by wgwj4809 on 01/08/17.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle aoSavedInstanceState) {
+        super.onCreate(aoSavedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
